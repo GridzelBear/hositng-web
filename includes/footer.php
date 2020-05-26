c37a383ce088f1e3f8e9a52cb70727de16550e1b
Ntd1611 <trungduc3326@gmail.com>
2020-05-25 22:10:55 -0400

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,81 @@
+<!-- Start About Section -->
+<footer id="about" class="offset">
+
+  <!--Start Theme Heading -->
+
+  <div class="container-fluid footer">
+    <div class="row pt-4 pb-5 justify-content-center">
+      <div class="footer-middle">
+        <div class="container ">
+          <div class="row">
+            <div class="col-md-3 col-sm-6">
+              <!--Column1-->
+              <div class="footer-pad">
+                <h4>Order</h4>
+                <ul class="list-unstyled">
+                  <li><a href="#">Order Page</a></li>
+                  <li><a href="#">Server Configuration And Order Summary Page</a></li>
+                  <li><a href="#">Review and Checkout</a></li>
+                  <li><a href="#">Check out</a></li>
+                  <li><a href="#">Invoice</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+              <!--Column1-->
+              <div class="footer-pad">
+                <h4>Control Panel</h4>
+                <ul class="list-unstyled">
+                  <li><a href="#">Manage Account</a></li>
+                  <li><a href="#">View Servers</a></li>
+                  <li><a href="#">Server Status</a></li>
+                  <li><a href="#">Order A Server</a></li>
+                  <li><a href="#">Rate Us</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+              <!--Column1-->
+              <div class="footer-pad">
+                <h4>Contact Us</h4>
+                <ul class="list-unstyled">
+                  <li><a href="#">Discord</a></li>
+                  <li><a href="#">News And Update</a></li>
+                  <li><a href="#">Workplace</a></li>
+                  <li><a href="#">FAQs</a></li>
+                  <li><a href="#">Information</a></li>
+                  <li>
+                    <a href="#"></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <h4>Follow Us</h4>
+              <ul class="social-network social-circle">
+                <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook-official" aria-hidden="true"></i></i></a></li>
+                <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i>
+                  </a></li>
+                <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter-square" aria-hidden="true"></i>
+                  </a></li>
+                <li><a href="#" class="icoInstagram" title="Instagram"><i class="fa fa-instagram" aria-hidden="true"></i></i></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 copy">
+              <p class="text-center">Copyright &copy; <script>
+                  document.write(new Date().getFullYear());
+                </script>, Web-Hosting. All rights reserved.</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+</footer>
+<!-- End About Section -->
